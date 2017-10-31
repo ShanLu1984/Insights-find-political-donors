8dff8ff4e7f39ded170157277bc57f62e7704b4b
ShanLu1984 <31110848+ShanLu1984@users.noreply.github.com>
2017-10-30 23:56:37 -0700

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Use this shell script to compile my test code and then execute it.
+# Use this shell script to compile and excute find_political_donors_test.cpp.
 #
 g++ -std=c++11 ./src/find_political_donors_test.cpp ./src/find_political_donors.cpp -o find_political_donors_test
 ./find_political_donors_test
