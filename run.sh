93bb324e2a75e151b642685ab9ac4dd2eae90743
ShanLu1984 <31110848+ShanLu1984@users.noreply.github.com>
2017-10-30 23:55:51 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+# Use this shell script to compile and excute find_political_donors_main.cpp.
 #
 g++ -std=c++11 ./src/find_political_donors_main.cpp ./src/find_political_donors.cpp -o find_political_donors
 ./find_political_donors ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
